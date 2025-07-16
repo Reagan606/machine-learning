934db9a37b27ef34ebceca1c579fccb517a37f29
Reagan606 <839804826@qq.com>
2025-07-16 20:23:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,11 +81,11 @@ if st.button("Predict"):
             """
         )
 
-   st.write(advice)     
-   explainer = shap.TreeExplainer(model)
-   shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
+    st.write(advice)     
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
 
-   shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
-   plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
+    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
+    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
 
-   st.image("shap_force_plot.png")
+    st.image("shap_force_plot.png")
